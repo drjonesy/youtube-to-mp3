f71215b8f4c0e6f112b3c8a1dfaf94ee3b9034a0
ryan <drjonesy@users.noreply.github.com>
2020-01-09 16:35:54 -0600

    Resolved Audio Issue. Uses MoviePy
    Uses Youtube-dl to download a low quality version of the video in Mp4 format.
    Then finds all the mp4 files within the same directory the file was written too.
    Extract the audio from each file, write the new audio Mp3 file, and remove the old Mp4 file.

--- YouTubeToMP3.py
+++ YouTubeToMP3.py
@@ -1,34 +1,84 @@
 from __future__ import unicode_literals
 import youtube_dl
 
-# this can either be a single video link or a playlist.
-# if it is a playlist, make sure to set noplaylist to True if you want to group the songs
-# youtube_url='https://www.youtube.com/watch?v=uT3SBzmDxGk' # single video
-youtube_url='https://www.youtube.com/watch?v=uT3SBzmDxGk&list=RDEM3VGq8Ysq0P67pRncwzau9A&start_radio=1' # playlist
-# define folder path and meta options for file, the folder path must exist
-# I had to create the 'test' folder
-file_path = 'test/%(title)s.mp3' 
+import moviepy.editor as mp
+import os
+import glob
 
+# ================= FUNCTIONS ==============
 def my_hook(d):
     if d['status'] == 'finished':
         print('Done downloading, now converting...')
 
+def PAUSE(s):
+    print(s)
+    input('....waiting...')
+
+
+# ------------------------------------------------------------------------------
+# ============= VIDEO URL ============================|| Video or Playlist
+# ------------------------------------------------------------------------------
+youtube_url='https://www.youtube.com/watch?v=uT3SBzmDxGk'
+# ==============================================================================
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+saveDir = os.path.join(basedir,'test')
+# ------------------------------------------------------------------------------
+# ============= Where to Save Files =======================|| Video or Playlist
+# ------------------------------------------------------------------------------
+mp4FileNameAndPath = os.path.join(saveDir, '%(title)s.mp4') 
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# ============= Youtube-dl  =======================|| Settings
+# ------------------------------------------------------------------------------
+
 ydl_opts = {
-    'format': 'mp3/worstvideo', # by selecting worstvideo the file size is reduced
-    'outtmpl': file_path,
+    # 'format': 'worstvideo', # by selecting worstvideo the file size is reduced
+    'format': 'mp4/worstvideo',
+    'outtmpl': mp4FileNameAndPath,
     'progess_hooks': [my_hook],
     'ignoreerrors': True,
     'noplaylist': False, # set to True for single video
-    'playliststart': 1,  # defines which video to start from in a playlist
-    'playlistend': 10,   # defines which video to end at in a playlist
+    # 'playliststart': 1,  # defines which video to start from in a playlist
+    # 'playlistend': 10,   # defines which video to end at in a playlist
 }
+# ==============================================================================
 
-with youtube_dl.YoutubeDL(params=ydl_opts) as ydl:
-    ydl.download(url_list=[youtube_url])
+if __name__ == "__main__":
+    
+    # ------------------------------------------------------------------------------
+    # ============= Download Mp4  =======================|| Video or Playlist
+    # ------------------------------------------------------------------------------
+    with youtube_dl.YoutubeDL(params=ydl_opts) as ydl:
+        ydl.download(url_list=[youtube_url])
+    # ==============================================================================
 
 
-# with youtube_dl.YoutubeDL(params=ydl_opts) as ydl:
-    # meta = ydl.extract_info(url=youtube_url, download=False)
+    # ------------------------------------------------------------------------------
+    # ============= Retrieve Meta Data  =======================|| Video or Playlist
+    # ------------------------------------------------------------------------------
+    # with youtube_dl.YoutubeDL(params=ydl_opts) as ydl:
+        # meta = ydl.extract_info(url=youtube_url, download=False)
 
-# for k,v in list(meta.items())[:12]:
-#     print(f"{k}: {v}")
+    # for k,v in list(meta.items())[:12]:
+    #     print(f"{k}: {v}")
+    # ==============================================================================
+
+
+    # ------------------------------------------------------------------------------
+    # ============= Covert to Mp3  =======================|| Video or Playlist
+    # ------------------------------------------------------------------------------
+    fileList = glob.glob(os.path.join(saveDir, '*.mp4') )
+
+
+
+    for filePath in fileList:
+        # change audio file extension
+        audioFilePath = f"{os.path.splitext(filePath)[0]}.mp3"
+        audio = mp.AudioFileClip(filename=filePath)
+        audio.write_audiofile(filename=audioFilePath)
+        # remove Mp4 file
+        os.remove(filePath)
+
